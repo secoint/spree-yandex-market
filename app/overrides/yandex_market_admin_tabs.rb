6c009a73f86e4c73bbe0adb331512079a5315c07
Roman Smirnov <roman@railsdog.com>
2012-01-11 23:51:20 +0400

    Typo fix

--- app/overrides/yandex_market_admin_tabs.rb
+++ app/overrides/yandex_market_admin_tabs.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
   :virtual_path => "layouts/admin",
   :name => "yandex_market_admin_tabs",
-  :insert_after => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
+  :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
   :text => "<%=  tab(:yandex_market, { :route => \"admin_yandex_market_settings\" }) %>",
   :disabled => false)
