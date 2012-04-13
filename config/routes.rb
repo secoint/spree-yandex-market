cca56c2f660f01404d443669c7dc0bae57687445
Sergey Rezvanov <sergey@rezvanov.info>
2012-04-13 11:43:21 +0600

    Left in the menu only exported files

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resource :yandex_market_settings do
       member do
-        match :general
-        match :currency
         match :export_files
-        match :ware_property
         get :run_export
       end
     end
