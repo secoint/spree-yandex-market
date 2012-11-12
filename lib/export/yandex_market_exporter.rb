1956b409699cdd9bf10ccd2f1bff8211f1fdcc5f
Andrey Nenashev <proudhamyak@gmail.com>
2012-11-12 14:20:58 +0400

    Fixed exporter

--- lib/export/yandex_market_exporter.rb
+++ lib/export/yandex_market_exporter.rb
@@ -112,7 +112,6 @@ module Export
           xml.param gender, :name => 'Пол' if gender.present?
           xml.param product.localized_age, :name => 'Возраст' if product.age
           xml.param product.picture_type, :name => 'Тип рисунка' if product.picture_type
-          xml.param product.size_table.standarted_size_table, :name => 'Размер' if product.size_table
           xml.market_category product.market_category if product.market_category
         end
       end
